fadab2a71f7740cb12efd514a3d1e13562e47fd9
alex7est <alexanderestrella82@gmail.com>
2024-01-23 17:18:12 -0500

    se separa las funciones al archivo utilitarios.js

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,36 +1,3 @@
-function recuperarTexto(idComponente){
-    let componente;
-    let valorIngresado;
-    componente = document.getElementById(idComponente);
-    valorIngresado = componente.value;
-    return valorIngresado;
-}
-function recuperarInt(idComponente){
-    let valorCaja = recuperarTexto(idComponente);
-    let valorEntero = parseInt(valorCaja);
-    return valorEntero;
-}
- function recuperarFloat(idComponente){
-    let valorCaja = recuperarTexto(idComponente);
-    let valorFloat = parseFloat(valorCaja);
-    return valorFloat;
-}
-function mostrarTexto(idComponente, mensaje) {
-    let componente;
-    componente = document.getElementById(idComponente);
-    componente. innerText = mensaje;
-}
-function mostrarImagen(idComponente, rutaImagen){
-    let componente;
-    componente = document.getElementById(idComponente) ;
-    componente.src = rutaImagen;
-}
-function mostrarTextoEnCaja(idComponente, mensaje) {
-    let componente;
-    componente=document.getElementById(idComponente) ;
-    componente.value = mensaje;
-}
-
 function saludar(){
     let nombre = recuperarTexto("txtNombre");
     let apellido = recuperarTexto("txtApellido");
