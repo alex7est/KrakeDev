b936c790d65d2c00714a39ae0f80b17812af4e9d
alex7est <alexanderestrella82@gmail.com>
2024-01-23 18:05:47 -0500

    valorIva se muestra correctamente

--- puntoDeVenta/puntoDeVenta.js
+++ puntoDeVenta/puntoDeVenta.js
@@ -47,6 +47,7 @@ calcularValorTotal = function(){
     mostrarTexto("lblDescuento",valorDescuento);
 
     //8. Invocar a calcularIVA y lo que devuelve guardar en la variable valorIVA
+    valorIVA = calcularIva(valorSubtotal-valorDescuento);
 	//   El IVA debe calcularse sobre el valor del subtotal menos el descuento
     //9. Mostrar el resultado en el componente lblValorIVA    
     /*
@@ -58,6 +59,8 @@ calcularValorTotal = function(){
                 IVA esperado: 5.832
             Si el caso de prueba es exitoso, hacer un commit
         */
+    mostrarTexto("lblValorIVA",valorIVA);
+
     //10. Invocar a calcularTotal y lo que devuelve guardar en la variable valorTotal
     //11. Mostrar el resultado en el componente lblTotal
      /*
