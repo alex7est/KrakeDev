40090176f18e96a582af612f6e884c7f20460699
alex7est <alexanderestrella82@gmail.com>
2024-02-05 21:38:08 -0500

    Ahorcado Paso 1

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,8 +1,26 @@
+let palabraSecerta;
+
 function esMayuscula(caracter){
     let code =  caracter.charCodeAt(0);
     if(code >= 65 && code <= 90){
         return true;
     } else {
         return false;
+    }
+}
+
+function guardarPalabra(){
+    let palabra = recuperarTexto("txtSecreta");
+    let error = false;
+    for(let i=0;i<5;i++){
+        if(!esMayuscula(palabra.charAt(i))){
+            error = true;
+        }
+    }
+    if (palabra.length != 5 || error){
+        alert("Ingrese una palabra de 5 letras mayusculas")
+    }else{
+        palabraSecerta = palabra;
+        console.log(palabraSecerta);
     }
 }
