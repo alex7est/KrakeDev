278a4443504d9b8924fccdcb73cd7bba84218c83
alex7est <alexanderestrella82@gmail.com>
2024-02-16 21:06:03 -0500

    Funcion guardar agregada

--- roles/roles.js
+++ roles/roles.js
@@ -50,3 +50,97 @@ function ejecutarNuevo(){
     habilitarComponente("btnGuardar");
     esNuevo = true;
 }
+
+function buscarEmpleado(cedula){
+    let empleado;
+    let EmpleadoEncontrado=null;
+    for (let i=0;i<empleados.length;i++){
+        empleado=empleados[i];
+        if(empleado.cedula == cedula){
+            EmpleadoEncontrado=empleado;
+            break;
+        }
+    }
+    return EmpleadoEncontrado;
+}
+
+function agregarEmpleado(empleado){
+    let resultado=buscarEmpleado(empleado.cedula); 
+    if(resultado==null){ 
+        empleados.push(empleado);
+        return true;
+    }else{ 
+        return false;
+    }
+}
+
+function guardar(){
+    let cedula = recuperarTexto("txtCedula");
+    let nombre = recuperarTexto("txtNombre");
+    let apellido = recuperarTexto("txtApellido");
+    let sueldo = recuperarFloat("txtSueldo");
+
+    let errorCedula = false;
+    if(cedula.length!=10){
+        errorCedula = true;
+    }else{
+        for (let i = 0; i < cedula.length; i++) {
+            if (cedula[i] < '0' || cedula[i] > '9') {
+                errorCedula = true;
+            }
+        }
+    }
+    if(errorCedula){
+        mostrarTexto("lblErrorCedula","Debe tener exactamente 10 digitos");
+    }else{
+        mostrarTexto("lblErrorCedula","");
+    }
+    
+    let errorNombre = false;
+    if(nombre.length<3 || nombre!=nombre.toUpperCase()){
+        errorNombre = true;
+    }
+    if(errorNombre){
+        mostrarTexto("lblErrorNombre","Minimo 3 letras, todas mayusculas");
+    }else{
+        mostrarTexto("lblErrorNombre","");
+    }
+
+    let errorApellido = false;
+    if(apellido.length<3 || apellido!=apellido.toUpperCase()){
+        errorApellido = true;
+    }
+    if(errorApellido){
+        mostrarTexto("lblErrorApellido","Minimo 3 letras, todas mayusculas");
+    }else{
+        mostrarTexto("lblErrorApellido","");
+    }
+
+    let errorSueldo = false;
+    if(sueldo<400 || sueldo>5000 || isNaN(sueldo)){
+        errorSueldo = true;
+    }
+    if(errorSueldo){
+        mostrarTexto("lblErrorSueldo","Un numero entre 400.00 y 5000.00");
+    }else{
+        mostrarTexto("lblErrorSueldo","");
+    }
+
+    if(!errorCedula && !errorNombre && !errorApellido && !errorSueldo){
+        if(esNuevo){
+            let empleado = {}
+            empleado.cedula = cedula;
+            empleado.nombre = nombre;
+            empleado.apellido = apellido;
+            empleado.sueldo = sueldo;
+            let guardado = agregarEmpleado(empleado);
+            if(guardado){
+                alert("EMPLEADO GUARDADO CORRECTAMENTE");
+                mostrarEmpleados();
+            }else{
+                alert("YA EXISTE UN EMPLEADO CON LA CEDULA: "+empleado.cedula);
+            }
+        }
+    }
+}
+
